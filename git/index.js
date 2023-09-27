b1267022f047ed560a7e591fbb2980529311b7e3
Nacho Serrano <ignaserrano@gmail.com>
2023-09-27 20:53:14 +0200

    Added comments

--- git/index.js
+++ git/index.js
@@ -10,6 +10,6 @@ console.log("==================");
 let salida = "";
 
 for (let i = 1; i <= 10; i++) {
-    salida += `5 x ${i} = ${5 * i}\n`;
+    salida += `5 x ${i} = ${5 * i}\n`; //Adding \n to make a new line
 }   
 console.log(salida);
