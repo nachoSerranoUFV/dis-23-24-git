35294365274a5d570e9a6bd1fd8ff0d4ee494998
Nacho Serrano <ignaserrano@gmail.com>
2023-09-28 19:51:16 +0200

    Trying to force a conflict

--- git/index.js
+++ git/index.js
@@ -14,8 +14,8 @@ for (let i = 1; i <= 10; i++) {
     salida += `5 x ${i} = ${5 * i}\n`;
 }   
 
-for (let i = 1; i <= 30; i++) {
-    salida_bis += `5 x ${i} = ${5 * i}\n`; //Comments by Nacho
-}   
+// for (let i = 1; i <= 30; i++) {
+//     salida_bis += `5 x ${i} = ${5 * i}\n`; //Comments by Nacho
+// }   
 console.log(salida);
 console.log(salida_bis);
